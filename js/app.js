69f595bc944fd7696c0ae23f365714ba0e86a0e7
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2017-06-03 00:41:31 +0200

    Fixed insults hash

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@
     var insultSize = insults.default.length;
     var now = (new Date()).getTime();
     var fullDaysSinceEpoch = Math.floor(now/8.64e7);
-    var selected = (insultSize % fullDaysSinceEpoch) -1;
+    var selected = insults.default[(fullDaysSinceEpoch % insultSize) -1];
 
-    $('.js-insult-text').text(insults.default[selected]);
+    $('.js-insult-text').text(selected);
   });
 
   $('.js-time').text(moment().format('LL'));
