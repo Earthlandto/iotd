cdf4e59d9648f0f41bbc2abdd0f736d5997d38b2
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2017-06-15 23:55:16 +0200

    Added insults by special day

--- js/app.js
+++ js/app.js
@@ -6,10 +6,7 @@
   $.getJSON('insults.json', function(data) {
     insults = data.insults;
 
-    var insultSize = insults.default.length;
-    var now = (new Date()).getTime();
-    var fullDaysSinceEpoch = Math.floor(now/MILLISECOND_PER_DAY);
-    var selected = insults.default[(fullDaysSinceEpoch % insultSize) -1];
+    var selected = getInsult(insults.default, insults.special);
 
     $('.js-insult-text').text(selected);
   });
@@ -30,13 +27,34 @@
   	$('.js-extra-list').removeClass('hide');
   });
 
+  /**
+   * Given the default and special list, returns the selected insult.
+   * If it exists a special insult for the current day, this will be selected,
+   * else, it returns a insult from default list by current date.
+   */
+  function getInsult(defaults, specials) {
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
- */
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+    var currentDateStr = moment().format('DD/MM/YYYY');
+
+    if (specials && specials[currentDateStr]) {
+      return specials[currentDateStr];
+    }
+
+    var insultSize = defaults.length;
+    var now = (new Date()).getTime();
+    var fullDaysSinceEpoch = Math.floor(now/MILLISECOND_PER_DAY);
+    return defaults[(fullDaysSinceEpoch % insultSize) -1];
+  }
+
+
+  /**
+   * Returns a random integer between min (inclusive) and max (inclusive)
+   * Using Math.round() will give you a non-uniform distribution!
+   */
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
 
 })();
